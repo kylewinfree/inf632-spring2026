abbe8b129b514f460141707addd3f8a2c02cf851
Kyle N. Winfree <kyle.winfree@nau.edu>
2026-01-29 10:42:04 -0700

    ready for disuccions

--- src/latex_course_documents.sh
+++ src/latex_course_documents.sh
@@ -15,7 +15,9 @@ directories=(
     "lecture-4-data-and-analysis"
     "lecture-5-agreements-correlations-regression"
     "research-project"
+    "week-1-discussion"
     "week-2-discussion"
+    "week-3-discussion"
 )
 
 # Let's instead just add all directories, assuming that there is a latex file in each
